1880cfdbd62963ad77a3dfc15f15f15f6eed2153
Ashwani Kumar Kushwaha <abic.ashwani@gmail.com>
2024-07-06 01:02:19 +0530

    axios instance added

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -3,8 +3,6 @@ import axios from "axios";
 import LocalStroage from "./localstroage";
 
 const token = LocalStroage() || "";
-
-console.log("token==", token);
 const axiosConfig = axios.create({
 	baseURL: "http://localhost:8081",
 	headers: {
@@ -14,17 +12,3 @@ const axiosConfig = axios.create({
 });
 
 export default axiosConfig;
-
-// import axios from "axios";
-
-// const instance = axios.create({
-//   baseURL : 'http://127.0.0.1:8000/api/',
-//   headers: {
-// //  Authorization: `<Your Auth Token>`,
-//     Content-Type: "application/json",
-//     timeout : 1000,
-//   },
-//   // .. other options
-// });
-
-// export default instance;
