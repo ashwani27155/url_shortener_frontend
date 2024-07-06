cf1ebaaf0307c594fb7afc95109bc05751e5c010
Ashwani Kumar Kushwaha <abic.ashwani@gmail.com>
2024-07-06 09:29:28 +0530

    chnages app title and fix token issue

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 import LocalStroage from "./localstroage";
-
 const token = LocalStroage() || "";
+console.log("token==", token);
 const axiosConfig = axios.create({
 	baseURL: "http://localhost:8081",
 	headers: {
